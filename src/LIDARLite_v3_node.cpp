31341dd2e19bd3cd2561fd30acd79eaecbde34c1
Brosnan Yuen <brosnanyuen@gmail.com>
2017-04-06 12:57:53 -0700

    Laserscan fix

--- src/LIDARLite_v3_node.cpp
+++ src/LIDARLite_v3_node.cpp
@@ -73,7 +73,7 @@ int32_t main(int argc, char *argv[])
 
 	LIDARdata LIDAR1;
 	LIDAR1.pub = LIDAR_pub;
-	LIDAR1.LIDARdata.header.frame_id = "laser";
+	LIDAR1.LIDARdata.header.frame_id = "base_link";
 	LIDAR1.LIDARdata.range_min = LIDAR_RANGE_MIN;
 	LIDAR1.LIDARdata.range_max = LIDAR_RANGE_MAX;	
 	LIDAR1.LIDARdata.angle_increment = (1.8/4.0);
@@ -119,7 +119,6 @@ int32_t main(int argc, char *argv[])
 	dirDRV8825(&NEMA17, DRV8825_FORWARD);
 
 	float range = 0.0;
-	double total_time = 0.0;
 	double dt = 0.0;
 	ros::Time startScan,startTrigger;
 	while (ros::ok())
@@ -149,8 +148,8 @@ int32_t main(int argc, char *argv[])
 			if (LIDAR1.LIDARdata.ranges.size() > 0)
 			{
 				LIDAR1.LIDARdata.angle_max = LIDAR1.LIDARdata.angle_min + ((((float)NEMA17.count) + 1.0)*((2.0*M_PI)/(800.0)));
-				LIDAR1.LIDARdata.time_increment = (float)((total_time)/(((double)NEMA17.count) + 1.0));
 				LIDAR1.LIDARdata.scan_time = (float)((LIDAR1.LIDARdata.header.stamp - startScan).toSec());
+				LIDAR1.LIDARdata.time_increment = (float)((LIDAR1.LIDARdata.scan_time)/(((double)NEMA17.count) + 1.0));
 				LIDAR1.pub.publish(LIDAR1.LIDARdata);
 			}
 			
@@ -170,11 +169,9 @@ int32_t main(int argc, char *argv[])
 			
 			LIDAR1.LIDARdata.ranges.clear();
 			LIDAR1.LIDARdata.angle_min = (NEMA17.count % 800 )*((2.0*M_PI)/(800.0));
-			total_time = 0.0;
 			continue;
 		}
 		stepDRV8825(&NEMA17);
-		total_time += dt;
 		LIDAR1.LIDARdata.ranges.push_back(range);
 	}
 
