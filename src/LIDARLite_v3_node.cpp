fb021972de49fc7c86e8822adb24b5b11e5c9f72
Brosnan Yuen <brosnanyuen@gmail.com>
2017-04-24 15:26:15 -0700

    add PointCloud

--- src/LIDARLite_v3_node.cpp
+++ src/LIDARLite_v3_node.cpp
@@ -23,7 +23,8 @@
 #include "include/LIDARLite_v3/LIDARLite_v3.h"
 #include "include/LIDARLite_v3/DRV8825.h"
 
-
+#include <std_msgs/Float32.h>
+#include <sensor_msgs/PointCloud2.h>
 
 int32_t main(int argc, char *argv[])
 {
@@ -32,6 +33,7 @@ int32_t main(int argc, char *argv[])
 	ros::init(argc, argv, "LIDARLite_v3");
 	ros::NodeHandle node;
 	ros::Publisher LaserScan_pub = node.advertise<sensor_msgs::LaserScan>("scan", 500);
+	ros::Publisher Cloud_pub = node.advertise<sensor_msgs::PointCloud2>("cloud_in", 500);
 	//ros::Subscriber sub = node.subscribe("LIDAR_command", 1000, IMU_commandCallback);
 	ros::Rate loop_rate(500);
 
@@ -73,7 +75,8 @@ int32_t main(int argc, char *argv[])
 	setStepMode(&NEMA17,1);
 	dirDRV8825(&NEMA17, DRV8825_FORWARD);
 
-	//Setup data
+
+	//Setup LaserScan data
 	sensor_msgs::LaserScan LIDARdata;
 	LIDARdata.header.frame_id = "laser";
 	LIDARdata.range_min = LIDAR_RANGE_MIN;
@@ -86,7 +89,40 @@ int32_t main(int argc, char *argv[])
 	float rangeArray[NEMA17.stepsPerCycle];
 	int32_t i;
 
-	std::cout << "step: " << NEMA17.stepsPerCycle << std::endl;
+
+
+	//Point cloud data
+	sensor_msgs::PointCloud2 Pointdata;
+	Pointdata.header.frame_id = "laser";
+	Pointdata.width  = NEMA17.stepsPerCycle;
+	Pointdata.height = 1;
+	Pointdata.fields.resize(3);
+	
+	Pointdata.fields[0].name = "x";
+	Pointdata.fields[1].name = "y";
+	Pointdata.fields[2].name = "z";
+
+	uint32_t offset = 0;
+	size_t index;
+	for (index = 0; index < Pointdata.fields.size(); ++index, offset += 4)
+	{
+		Pointdata.fields[index].count    = 1;
+		Pointdata.fields[index].offset   = offset;
+		Pointdata.fields[index].datatype = sensor_msgs::PointField::FLOAT32;
+	}
+	size_t x_data_offset = Pointdata.fields[0].offset ;
+	size_t y_data_offset = Pointdata.fields[1].offset ;
+
+	Pointdata.point_step = offset;
+	Pointdata.row_step = Pointdata.point_step * Pointdata.width;
+	Pointdata.data.resize( Pointdata.row_step);
+	std::fill(Pointdata.data.begin(), Pointdata.data.end(), 0);
+	Pointdata.is_bigendian = false;
+	Pointdata.is_dense = true;
+	float pos_x,pos_y,theta;
+
+
+	std::cout << "step per cycle: " << NEMA17.stepsPerCycle << std::endl;
 	float range = 0.0;
 	ros::Time startScan,endScan;
 	while (ros::ok())
@@ -124,9 +160,16 @@ int32_t main(int argc, char *argv[])
 		for (i=0;i<NEMA17.stepsPerCycle;++i)
 		{
 			LIDARdata.ranges[i] = rangeArray[i];
+	
+			theta = (i*NEMA17.angle_increment) - M_PI;
+			pos_x = (float)((rangeArray[i])*cos(theta));
+			pos_y = (float)((rangeArray[i])*sin(theta));
+			memcpy(&Pointdata.data[i * Pointdata.point_step + x_data_offset], &pos_x, sizeof(float));
+			memcpy(&Pointdata.data[i * Pointdata.point_step + y_data_offset], &pos_y, sizeof(float));
 		}
 		LaserScan_pub.publish(LIDARdata);
-
+		Cloud_pub.publish(Pointdata);
+		
 		NEMA17.current_angle = -M_PI;
 		NEMA17.count = 0;
 	}
